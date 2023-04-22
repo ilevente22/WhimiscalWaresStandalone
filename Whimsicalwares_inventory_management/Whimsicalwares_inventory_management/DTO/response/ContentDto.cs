0faebdddee7553a20ff66267d917a90bd00e7bcd
balintkss <dellbalint13@gmail.com>
2023-04-22 17:33:01 +0200

    Adatok betöltése a termék apiról

---
+++ Whimsicalwares_inventory_management/Whimsicalwares_inventory_management/DTO/response/ContentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whimsicalwares_inventory_management.DTO.response
+{
+    public class ContentDto
+    {
+        public List<ProductDto> Products { get; set; }
+    }
+}
---
+++ Whimsicalwares_inventory_management/Whimsicalwares_inventory_management/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+using Whimsicalwares_inventory_management.DTO.response;
+
+namespace Whimsicalwares_inventory_management
+{
+    public partial class Form1 : Form
+    {
+        BindingSource bindingSource = new BindingSource();
+        private static String BASE_URL = "http://20.234.113.211:8094//DesktopModules/Hotcakes/API/rest/v1/";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+
+        }
+        public async void LoadData()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BASE_URL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("products?key=1-af60ed8e-94ff-4da2-a167-8b716ab5629a");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                //var  A= JsonConvert.DeserializeObject<UpdateHotelInfo>(data);
+                // List<UpdateHotelInfo> a = response.Content.ReadAsAsync<List<UpdateHotelInfo>>().Result;
+                var result = JsonConvert.DeserializeObject<ProductsResponseRootDto>(data);
+                dataGridView1.DataSource = result.Content.Products;
+            }
+        }
+    }
+}
