ee54c0dd62beb1bd640a236f23e05bc34dbd7690
ilevente22 <levente.illes@stud.uni-corvinus.hu>
2023-05-18 16:15:16 +0200

    Átírható datagridview javítása

--- Whimsicalwares_inventory_management/Whimsicalwares_inventory_management/Form1.Designer.cs
+++ Whimsicalwares_inventory_management/Whimsicalwares_inventory_management/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(39, 77);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(1402, 432);
